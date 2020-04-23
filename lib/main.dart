735a8e8d274d903b66203f96d662c59406f3a44e
mahakmakharia <makhariamahak@gmail.com>
2020-04-23 13:32:12 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(StartupNameGeneratorApp());
+}
+
+class StartupNameGeneratorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
